654b7a780c9b6419eab8ba4e512e3d77352353ce
Thaís Tozatto <thais.c.tozatto@gmail.com>
2021-04-02 18:30:29 -0300

    Adicionar arquivos de projeto.

---
+++ Startup.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Alura_MVC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // AddTransient - Objetos transitórios são sempre diferentes; uma nova instância é fornecida cada vez que o objeto é solicitado.
+            // AddScoped - Objetos com escopo são os mesmos em uma requisições, mas diferentes entre requisições diferentes.
+            // AddSingleton - Objetos singleton são os mesmos para todas as requisições.
+
+            services.AddControllersWithViews();
+            services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Pedido}/{action=Carrossel}/{id?}");
+            });
+        }
+    }
+}
